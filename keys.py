cac0140fded29b60e63b86cdf4eec31823b95117
Eric <183417891+McCire@users.noreply.github.com>
2024-11-21 13:24:37 +0100

    Doing optimization to main script

--- keys.py
+++ keys.py
@@ -9,34 +9,29 @@ COMBINATIONS4 = [{keyboard.Key.alt, keyboard.KeyCode(char="C")}]
 #some needed variables
 keyboards = Controller()
 current = set()
-#ñ,Ñ
-def executen():
-    keyboards.type("ñ")
-def executeN():
-    keyboards.type("Ñ")
-#Ç,ç
-def executeç():
-    keyboards.type("ç")
-def executeÇ():
-    keyboards.type("Ç")
+#ñ,Ñ,Ç,ç
+def execute(char):
+    keyboards.type(char)
+
 #To do; optimise this crap, lots of bucle, possible if else statement for second key pressed when pressing alt+"{ç,ñ,Ñ,Ç}"?
 def on_press(key):
     if any([key in COMBO for COMBO in COMBINATIONS]):
         current.add(key)
         if any(all(k in current for k in COMBO) for COMBO in COMBINATIONS):
-            executen()
+            execute("ñ")
     elif any([key in COMBO for COMBO in COMBINATIONS2]):
         current.add(key)
         if any(all(k in current for k in COMBO) for COMBO in COMBINATIONS2):
-            executeN()
+            execute("Ñ")
     elif any([key in COMBO for COMBO in COMBINATIONS3]):
         current.add(key)
         if any(all(k in current for k in COMBO) for COMBO in COMBINATIONS3):
-            executeç()
+            execute("ç")
     elif any([key in COMBO for COMBO in COMBINATIONS4]):
         current.add(key)
         if any(all(k in current for k in COMBO) for COMBO in COMBINATIONS4):
-            executeÇ()
+            execute("Ç")
+
 def on_release(key):
     if any([key in COMBO for COMBO in COMBINATIONS]):
         current.remove(key)
